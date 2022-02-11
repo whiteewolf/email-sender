0a46599c5004469e1c43443e0a0e59585855adbc
whiteewolf <arog0660@gmail.com>
2022-02-12 00:20:36 +0200

    simply done lol

---
+++ index.js
@@ -0,0 +1,24 @@
+const fs = require("fs")
+const rawdata = fs.readFileSync('./accounts.json')
+const data = JSON.parse(rawdata)
+
+console.log(data)
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.question('What is your email? : ', function(sender) {
+    rl.question('Who do you want to send this to ? (Please provide email) : ', function (email) {
+        rl.question('Input your message : ', function (msg) {
+            let obj = [];
+            data.push({email: email, sender: sender, message: msg, date: new Date()})
+            fs.writeFile('./accounts.json', JSON.stringify(data), (err) => {})
+            rl.close();
+        });
+    });
+});
+rl.on('close', function () {
+//   console.log('\nBYE BYE !!!');
+//   process.exit(0);
+});
